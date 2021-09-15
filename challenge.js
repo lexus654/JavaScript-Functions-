a258bbba2f69f5a1b5a58f1c29f9b351db5550ff
lexus654 <ronald.laz@tup.edu.ph>
2021-09-15 10:20:19 +0800

    This is a compilation

---
+++ challenge.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Challenge 1 Return sum of two numbers
+
+const addition = (a, b) => console.log(a + b);
+
+addition(1, 4);
+
+// challenge 2 Return the first value in an array
+
+const firstValue = (arr) => console.log(arr[0]);
+
+firstValue([1, 5, 2, 7, 3]);
+
+// Challenge 3 Return the area of a triangle
+const areaTriangle = (base, height) => console.log((base * height) / 2);
+areaTriangle(23, 5);
