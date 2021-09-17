e786c22b45d58b1bc237620793ae9816580bd6dd
lexus654 <ronald.laz@tup.edu.ph>
2021-09-17 20:51:02 +0800

    Challenge 13 done

--- challenge.js
+++ challenge.js
@@ -148,3 +148,21 @@ const whichIsLarger = function (f, g) {
     return "neither";
   }
 };
+
+// challenge 13
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  compareAge(other) {
+    if (this.age < other.age) {
+      return `${other.name} is older than me.`;
+    } else if (this.age > other.age) {
+      return `${other.name} is younger than me.`;
+    } else if (this.age === other.age) {
+      return `${other.name} is the same age as me.`;
+    }
+  }
+}
